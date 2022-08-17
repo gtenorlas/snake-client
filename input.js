a15edeae499cfb09d074efcd2bd6fd6a6c19b8c0
Gene Tenorlas <tenorlas@sheridancollege.ca>
2022-08-17 02:43:39 +0000

    Send string to the server based off keystroke from stdin to control the snake

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+// Stores the active TCP connection object.
+let connection;
 
 /*
 callback function to handle the keys pressed by the user
@@ -6,11 +8,23 @@ const handleUserInput = function(key) {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
     process.exit();
+  } else if (key === 'w') {
+
+    //send string "Move: up" to the server
+    connection.write("Move: up");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === 's') {
+    connection.write("Move: down");
+  } else if (key === 'd') {
+    connection.write("Move: right");
   }
 };
 
 // setup interface to handle user input from stdin
-const setupInput = function() {
+const setupInput = function(conn) {
+  //store server object to connection
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
