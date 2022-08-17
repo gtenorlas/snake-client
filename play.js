e4fed77d673781bc10e5d89414dff2e1a5f1edf7
Gene Tenorlas <tenorlas@sheridancollege.ca>
2022-08-17 01:44:01 +0000

    Implement setupInput and handleUserInput to create stdin and handle event for keys pressed by the user. Allow exit for ctrl+c.

--- play.js
+++ play.js
@@ -1,6 +1,30 @@
 //import a module using a shorthand version
 const { connect } = require("./client");
 
+/*
+callback function to handle the keys pressed by the user
+*/
+const handleUserInput = function(key) {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
 
+// setup interface to handle user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  //handle event for keyboard input
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+setupInput();
 connect();
 console.log("Connecting...");
+
