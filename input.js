e13e14e3b7006aa761734ddabf1fccb7ba3dcebd
Gene Tenorlas <tenorlas@sheridancollege.ca>
2022-08-17 03:03:04 +0000

    Allow keys z and x to create chat bubbles beside the snake

--- input.js
+++ input.js
@@ -4,7 +4,7 @@ let connection;
 /*
 callback function to handle the keys pressed by the user
 */
-const handleUserInput = function(key) {
+const handleUserInput = function (key) {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
     process.exit();
@@ -18,11 +18,15 @@ const handleUserInput = function(key) {
     connection.write("Move: down");
   } else if (key === 'd') {
     connection.write("Move: right");
+  } else if (key === 'z') {
+    connection.write("Say: I am great");
+  } else if (key === 'x') {
+    connection.write("Say: Quit!");
   }
 };
 
 // setup interface to handle user input from stdin
-const setupInput = function(conn) {
+const setupInput = function (conn) {
   //store server object to connection
   connection = conn;
   const stdin = process.stdin;
